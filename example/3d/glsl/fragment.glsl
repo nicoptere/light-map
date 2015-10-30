7e34aaff9836b282ad1f3ff8acc9da76124e5773
nicolas barradeau <barradeau@gmail.com>
2015-10-30 18:19:48 +0100

    update controls example

--- example/3d/glsl/fragment.glsl
+++
@@ -1,9 +0,0 @@
-uniform sampler2D diffuse;
-uniform sampler2D heightmap;
-varying vec2 vUv;
-void main(){
-
-    //gl_FragColor = texture2D( diffuse, vUv );
-    gl_FragColor = texture2D( heightmap, vUv );
-
-}
